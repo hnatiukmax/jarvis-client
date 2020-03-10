8e63888d1bc55468b9b947acc4faa960ecb8b23c
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-03-10 17:23:53 +0200

    Added custom toolbar. Added info dialog.

--- app/src/main/java/com/bugmakers/jarvistime/presentation/pages/login/LogInActivity.kt
+++ app/src/main/java/com/bugmakers/jarvistime/presentation/pages/login/LogInActivity.kt
@@ -1,5 +1,7 @@
 package com.bugmakers.jarvistime.presentation.pages.login
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -8,10 +10,18 @@ import com.bugmakers.jarvistime.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
 
+    private val viewModel = LogInActivityViewModel()
+
     private lateinit var binding: ActivityLogInBinding
+
+    companion object {
+        fun getIntent(context : Context) = Intent(context, this::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_log_in)
     }
+
+
 }
