749b84c72725bba1b2c8ef51654f896e6370db2c
Artem Rubansky <temi439@gmail.com>
2020-03-10 18:52:47 +0200

    added matrix fragment

--- app/src/main/java/com/bugmakers/jarvistime/presentation/pages/main/MainActivity.kt
+++ app/src/main/java/com/bugmakers/jarvistime/presentation/pages/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bugmakers.jarvistime.presentation.pages.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.bugmakers.jarvistime.R
 import com.bugmakers.jarvistime.databinding.ActivityMainBinding
 
@@ -13,6 +14,28 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.mainToolbar.setLeftActionAsBackButton(true)
+
+        addFragmentWitoutBackStack(MatrixFragment(), MatrixFragment.TAG)
+//        binding.mainToolbar.setLeftActionAsBackButton(true)
+    }
+
+    fun addFragmentWitoutBackStack(fragment : Fragment, tag : String) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment, tag)
+            .commit()
+    }
+
+    fun addFragment(fragment : Fragment, tag : String) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment, tag)
+            .addToBackStack(tag)
+            .commit()
+    }
+
+    fun replaceFragment(fragment : Fragment, tag : String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment, tag)
+            .addToBackStack(tag)
+            .commit()
     }
 }
