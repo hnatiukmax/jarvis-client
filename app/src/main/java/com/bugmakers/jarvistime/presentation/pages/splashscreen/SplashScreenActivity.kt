ee1435e41489564c134d1ad465ae24c140367c2a
Artem Rubansky <temi439@gmail.com>
2020-03-11 14:41:17 +0200

    create layout for main activity

--- app/src/main/java/com/bugmakers/jarvistime/presentation/pages/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/bugmakers/jarvistime/presentation/pages/splashscreen/SplashScreenActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bugmakers.jarvistime.presentation.extensions.goTo
 import com.bugmakers.jarvistime.presentation.pages.introduction.IntroductionActivity
+import com.bugmakers.jarvistime.presentation.pages.main.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        goTo(IntroductionActivity::class.java)
+        goTo(MainActivity::class.java)
         finish()
     }
 }
